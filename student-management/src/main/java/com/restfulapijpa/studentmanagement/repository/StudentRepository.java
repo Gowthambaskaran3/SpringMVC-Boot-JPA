f43489891f19e463e515e4b36c78bd559484caf6
Gowthambaskaran3 <89999850+Gowthambaskaran3@users.noreply.github.com>
2021-11-06 19:06:39 +0530

    Done PoC 3 and 4
    We have two microservices right now? Consider communicating with each other and one microservice is down and you need to handle fault tolerance in other microservice
    
    Write custom exception in spring boot application.. say like you are getting resource not found , bad request. can you guys pls write code how to handle such exception in microservices

---
+++ student-management/src/main/java/com/restfulapijpa/studentmanagement/repository/StudentRepository.java
@@ -0,0 +1,58 @@
+package com.restfulapijpa.studentmanagement.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.restfulapijpa.studentmanagement.entity.Student;
+
+@Repository
+@Transactional
+public class StudentRepository {
+	
+	@Autowired
+	EntityManager em;
+	
+	public List<Student> find() {
+		
+		@SuppressWarnings("unchecked")
+		List<Student> student = em.createNativeQuery("Select * from Student").getResultList();
+		return student;
+		
+	}
+	
+	public Student findById(Long id) {
+		return em.find(Student.class, id);
+	}
+	
+	@SuppressWarnings("null")
+	public Student save(Student student){
+		if(student.getId() == null) {
+			
+			em.persist(student);
+			System.out.println(student);
+		}
+		else {
+			em.merge(student);
+		}
+		return student;
+	}
+	
+	public void deleteById(Long id) {
+		Student student = findById(id);
+		em.remove(student);
+	}
+	
+	public void update(Student student) {
+		
+		em.merge(student);
+		
+	}
+
+
+	
+}
